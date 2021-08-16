84fef63ff93bedb8e37dbb595f29ef7fbf20b0e8
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2021-08-16 16:21:43 -0500

    Update

---
+++ Enums/ModerationType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBlogProject.Enums
+{
+    public enum ModerationType
+    {   
+        [Description("Political Propaganda")]
+        Political,
+        [Description("Inappropriate Language")]
+        Language,
+        [Description("Drug refferences")]
+        Drugs,
+        [Description("Threatening speech")]
+        Threatening,
+        [Description("Sexual content")]
+        UnwantedComments,
+        [Description("Hate Speech")]
+        HateSpeech,
+        [Description("Targeted Shaming")]
+        Shaming
+    }
+}
