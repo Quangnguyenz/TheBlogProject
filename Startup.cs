fc85b9ab0123475eb2e7eeb80a3e32387a847eb7
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2021-09-19 12:41:21 -0500

    Update

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace TheBlogProject
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseNpgsql(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                    ConnectionService.GetConnectionString(Configuration)));
             
             services.AddDatabaseDeveloperPageExceptionFilter();
 
